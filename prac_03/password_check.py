5ef742485f9b34e5717a22cd4a447c9cd3108fb0
jclim <jclim1007@gmail.com>
2020-08-27 11:08:38 +1000

    Refactor password_check.py to use functions

--- prac_03/password_check.py
+++ prac_03/password_check.py
@@ -1,14 +1,28 @@
 PASSWORD_PLACEHOLDER = "*"
 
-minimum_password_length = int(input("Please enter the minimum password length: "))
-raw_password = input("Please enter your password: ")
-if len(raw_password) >= minimum_password_length:
-    raw_password_valid = True
-else:
-    raw_password_valid = False
-while not raw_password_valid:
-    print("That does not meet the minimum length")
+
+def main():
+    minimum_password_length = int(input("Please enter the minimum password length: "))
+    raw_password = get_password(minimum_password_length)
+    print_hidden_password(raw_password)
+
+
+def print_hidden_password(raw_password):
+    print(f"Password: {len(raw_password) * PASSWORD_PLACEHOLDER}")
+
+
+def get_password(minimum_password_length):
     raw_password = input("Please enter your password: ")
     if len(raw_password) >= minimum_password_length:
         raw_password_valid = True
-print(f"Password: {len(raw_password) * PASSWORD_PLACEHOLDER}")
+    else:
+        raw_password_valid = False
+    while not raw_password_valid:
+        print("That does not meet the minimum length")
+        raw_password = input("Please enter your password: ")
+        if len(raw_password) >= minimum_password_length:
+            raw_password_valid = True
+    return raw_password
+
+
+main()
