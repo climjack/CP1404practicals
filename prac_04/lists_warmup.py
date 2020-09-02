f1374c3032009f4c3bdc5536c5abcdd1e395a79e
jclim <jclim1007@gmail.com>
2020-09-02 15:56:54 +1000

    Add completed lists_warmup.py

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,15 @@
+numbers = [3, 1, 4, 1, 5, 9, 2]
+# 3
+# 2
+# 1
+# 3, 1, 4, 1, 5, 9, 2
+# 1, 5
+# True
+# False
+# False
+# [[3, 1, 4, 1, 5, 9, 2], [6, 5, 3]]
+
+numbers[0] = "10"
+numbers[-1] = 1
+numbers[2:]
+9 in numbers
